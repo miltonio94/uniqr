7de72aded56c79d54d0611e79fd9de9072f377e5
Milton <miltonio94@gmail.com>
2023-08-06 10:48:23 +0000

    finished

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::{
     error::Error,
     fmt::format,
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self, BufRead, BufReader, Write},
 };
 
 type MyResult<R> = Result<R, Box<dyn Error>>;
@@ -49,15 +49,38 @@ pub fn get_args() -> MyResult<Config> {
 
 pub fn run(config: Config) -> MyResult<()> {
     let mut file = open(&config.input_file).map_err(|e| format!("{}: {}", config.input_file, e))?;
-    let mut line = String::new();
+    let mut current = String::new();
+    let mut previous = String::new();
+    let mut count: u64 = 0;
+    let mut out = create_file(&config.output_file)?;
+
+    let mut print = |count: u64, content: &str| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out, "{:>4} {}", count, content)?;
+            } else {
+                write!(out, "{}", content)?;
+            }
+        }
+        Ok(())
+    };
+
     loop {
-        let bytes = file.read_line(&mut line)?;
+        let bytes = file.read_line(&mut current)?;
         if bytes == 0 {
             break;
         }
-        print!("{}", line);
-        line.clear();
+
+        if current.trim_end() != previous.trim_end() {
+            print(count, &previous);
+            previous = current.clone();
+            count = 0;
+        }
+        count += 1;
+        current.clear();
     }
+
+    print(count, &previous);
     Ok(())
 }
 
@@ -65,5 +88,12 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn create_file(file_name: &Option<String>) -> MyResult<Box<dyn Write>> {
+    match file_name {
+        Some(file_name) => Ok(Box::new(File::create(file_name)?)),
+        _ => Ok(Box::new(io::stdout())),
     }
 }
